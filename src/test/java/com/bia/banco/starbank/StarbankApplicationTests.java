77108499cad4c4287540f14d23b6786a05435c75
BEATRIZ SOARES <beatriz.soares358@gmail.com>
2025-09-05 12:30:26 -0300

    criando repo

---
+++ src/test/java/com/bia/banco/starbank/StarbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bia.banco.starbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
